fc02177b497daddbe7399750dcf52e64cce22074
SilvaCris <crislaine.silvva@gmail.com>
2024-02-08 01:44:06 -0300

    create components

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter } from "react-router-dom"
+import Footer from "./componentes/Footer"
+import Menu from "./componentes/Menu"
+
+function App() {
+ 
+   
+
+  return (     
+   <BrowserRouter>
+
+      <Menu/>
+      <Footer/>
+
+      </BrowserRouter>
+      
+      
+  
+  )
+}
+
+export default App
---
+++ src/componentes/Footer/index.jsx
@@ -0,0 +1,13 @@
+import styles from './footer.module.css'
+
+function Footer() {
+  return(
+    <footer className={styles.footer}>
+      <p>
+        Criado por Crislaine Silva 👩🏽‍💻
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
